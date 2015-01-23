b6257f5df7bb93735a87431e4dc609efe9d93846
lonewolf <nikhilkalige@gmail.com>
2015-01-23 00:16:04 -0700

    Fix sass task errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require("gulp");
 var browser_sync = require("browser-sync");
-var sass = require("gulp-sass");
+var sass = require("gulp-ruby-sass");
 var filter = require("gulp-filter");
+var plumber = require('gulp-plumber');
+var colors = require("colors");
 var reload = browser_sync.reload;
 
 var paths = {
@@ -10,11 +12,27 @@ var paths = {
 }
 
 /**
+ * Plumber error handler
+ */
+var error_handler = function(err) {
+    console.log('[SASS] '.bold.magenta + err.message.bold.red);
+    this.emit('end');
+};
+
+/**
  * Sass Task
  */
 gulp.task("sass", function() {
+    console.log('[sass]'.bold.magenta + ' Compiling Sass');
     return gulp.src(paths.scss)
-        .pipe(sass({sourcemap: true}))
+        .pipe(plumber({
+            errorHandler: error_handler
+        }))
+        .pipe(sass({
+            sourcemapPath: paths.css,
+            style: "expanded",
+            lineNumbers: true
+        }))
         .pipe(gulp.dest(paths.css))
         .pipe(filter("**/*.css"))
         .pipe(browser_sync.reload({stream: true}));
