f7f26b36211fd19a4f215be33cc8d8d46a38e0a5
lonewolf <nikhilkalige@gmail.com>
2015-01-26 00:32:47 -0700

    Add production based tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,21 @@ var filter = require("gulp-filter");
 var plumber = require('gulp-plumber');
 var colors = require("colors");
 var reload = browser_sync.reload;
+var minify_css = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var gulpif = require('gulp-if');
+var lazypipe = require('lazypipe');
+var del = require("del");
+var argv = require('yargs').argv;
+
+var production = !!(argv.production);
 
 var paths = {
     scss: "./scss/index.scss",
     scss_glob: "./scss/**/*.scss",
-    css: "./static/css"
+    production: "./pelican-theme",
+    development: ".",
+    css: "/static/css",
 }
 
 /**
@@ -19,6 +29,15 @@ var error_handler = function(err) {
     console.log('[SASS] '.bold.magenta + err.message.bold.red);
     this.emit('end');
 };
+
+/**
+ * Css minify Pipe
+ */
+var css_minify_pipe = lazypipe()
+    .pipe(minify_css)
+    .pipe(rename, 'index.min.css')
+    .pipe(gulp.dest, (paths.production + paths.css));
+
 
 /**
  * Sass Task
@@ -34,10 +53,25 @@ gulp.task("sass", function() {
             style: "expanded",
             lineNumbers: true
         }))
-        .pipe(gulp.dest(paths.css))
+        .pipe(gulp.dest(paths.development + paths.css))
         .pipe(filter("**/*.css"))
-        .pipe(browser_sync.reload({stream: true}));
+        .pipe(browser_sync.reload({stream: true}))
+        .pipe(gulpif(production, css_minify_pipe()));
 });
+
+/**
+ * Copy static files
+ */
+gulp.task("copy", function() {
+    del([paths.production + "**/*"]);
+    gulp.src(paths.development + "/static/fonts/**/*")
+        .pipe(gulp.dest(paths.production + "/static/fonts/"));
+    gulp.src(paths.development + "/static/images/**/*")
+        .pipe(gulp.dest(paths.production + "/static/images/"));
+    gulp.src(paths.development + "/templates/**/*")
+        .pipe(gulp.dest(paths.production + "/templates/"));
+});
+
 
 /**
  * Start server
